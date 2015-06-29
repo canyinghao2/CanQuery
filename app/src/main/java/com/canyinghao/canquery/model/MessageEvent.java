db27fa71fd72a0b377d2e5c75a16453dcdf1f8df
yangjian <yangjian@showstart.com>
2015-06-29 19:59:29 +0800

    delete

--- app/src/main/java/com/canyinghao/canquery/model/MessageEvent.java
+++
@@ -1,9 +0,0 @@
-package com.canyinghao.canquery.model;
-
-public class MessageEvent {
-    public  String message;
-
-    public MessageEvent(String message) {
-        this.message = message;
-    }
-}
